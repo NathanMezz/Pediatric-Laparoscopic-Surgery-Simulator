aae250130789173ac5a33b7d7088038f432591f7
NathanMezz <nathan.mezzomo@carleton.ca>
2022-11-28 13:00:03 -0500

    Added coordinate label to centroid of contours

--- Pediatric Laparoscopic Surgery Simulator/HSV.py
+++ Pediatric Laparoscopic Surgery Simulator/HSV.py
@@ -85,6 +85,8 @@ while True:
     cv2.moveWindow('My Object', int(width / 2), int(height))
     myMaskSmall = cv2.resize(myMask, (int(width / 2), int(height / 2)))
 
+
+
     coord = cv2.findNonZero(myMask)
     contours, _ = cv2.findContours(myMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -92,16 +94,16 @@ while True:
     for cnt in contours:
         area = cv2.contourArea(cnt)
 
-        '''   if cv2.contourArea(cnt) > cv2.contourArea(max_contour):
-            max_contour = cnt
-
-        cnt = max_contour
-        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-        x,y,w,h=cv2.boundingRect(approx)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 4)'''
-
-        if area > 100:
+        if area > 200:
+            M = cv2.moments(cnt)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
             cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+            cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+            coords = str(cX) + ", " + str(cY)
+            cv2.putText(frame, coords, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
 
     cv2.imshow('My Mask', myMaskSmall)
     cv2.moveWindow('My Mask', 0, height)

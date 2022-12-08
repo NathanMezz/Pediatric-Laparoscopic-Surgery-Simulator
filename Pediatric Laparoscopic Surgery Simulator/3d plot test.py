a6d5acf1501caca80808b93988d1b1fc0f3fd243
NathanMezz <nathan.mezzomo@carleton.ca>
2022-12-08 14:17:22 -0500

    Update 3d plot test.py

--- Pediatric Laparoscopic Surgery Simulator/3d plot test.py
+++ Pediatric Laparoscopic Surgery Simulator/3d plot test.py
@@ -5,10 +5,15 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
 x_arr = np.array(range(0, 100))
 y_arr = np.array(range(0, 100))
 z_arr = np.array(range(0, 100))
 
 ax.plot3D(x_arr,y_arr,z_arr, 'blue')
 
+ax.scatter3D(x_arr, y_arr, z_arr, c ='red');
 plt.show()
